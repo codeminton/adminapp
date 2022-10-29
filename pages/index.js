b217b829d472e83a39ff7ecf27834071ed880c43
ugur_revy <ugur@revy.com.tr>
2022-10-29 23:38:02 +0300

    underline

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <h1 className="text-3xl font-bold underline">
+    <h1 className="text-3xl font-bold">
       Hello world!
     </h1>
   )
