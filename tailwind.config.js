8c7394fc988983bf2148dca859dc1af964244c6a
ugur_revy <ugur@revy.com.tr>
2022-10-29 23:58:11 +0300

    added side menu

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,9 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    fontFamily: {
+      'sans': ['Poppins', 'Arial', 'sans-serif'],
+    },
     extend: {},
   },
   plugins: [],
